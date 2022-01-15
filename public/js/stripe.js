54e5533f62d4d9d85a5e7fe1febaba5c4006b1e9
yassineKhouaja <yassinekhouaja@gmail.com>
2022-01-15 19:32:44 +0100

    initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import { showAlert } from './alerts';
+
+import axios from 'axios';
+
+const stripe = Stripe(
+  'pk_test_51KICvAGjIW2PDq0gE47AhSFEfFF0f3lus9aXrWisosTxLKiAqIi5jhQKao1X1ApA6D0O9RATQUNZxnsSFzxQW5kH00JtYPUCFw'
+);
+
+export const bookTour = async tourId => {
+  // get checkout session from API
+  try {
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+    console.log(session);
+  } catch (error) {
+    showAlert('error', err);
+  }
+
+  // create checkout
+};
