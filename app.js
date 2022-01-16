532b40854d02b26d4ee58fd8701404696a84b0e8
yassineKhouaja <yassinekhouaja@gmail.com>
2022-01-16 10:54:19 +0100

    add cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,6 +24,11 @@ app.enable('trust proxy');
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+// implement cors
+app.use(cors());
+
+app.options('*', cors());
 
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
